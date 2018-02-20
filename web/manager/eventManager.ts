756bb8997933bb2b9b79a3d6b086bd6711d09044
Rajeev Sathish <rajeev.sathish@tarento.com>
2018-02-20 12:54:44 +0530

    Changes for EventBus and packageJson

--- web/manager/eventManager.ts
+++ web/manager/eventManager.ts
@@ -7,7 +7,7 @@ export class EventManager {
     addEventListener(type: string, callback: any, scope: any) {
         eb.addEventListener(type, callback, scope)
     }
-    dispatchEvent(type: string, data: any, target: any) {
+    dispatchEvent(type: string, data?: any, target?: any) {
         eb.dispatch(type, target);
     }
     removeEventListener(type: string, callBack: any, scope: any) {
