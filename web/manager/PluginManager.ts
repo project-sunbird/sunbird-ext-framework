83b1649ed7b4b3948a759ed83e897fc6bc9d1cb4
Rajeev Sathish <rajeev.sathish@tarento.com>
2018-03-01 13:22:55 +0530

    Issue #000 refactor: Framework changes

--- web/manager/PluginManager.ts
+++ web/manager/PluginManager.ts
@@ -10,12 +10,22 @@ export interface Manifest {
     statePath: string,
     pluginFolderName: string,
 }
+export interface FrameworkConfig {
+    pluginBasePath: string;
+}
+export interface IClientPluginConstructor {
+    new(config: any, manifest: Manifest)
+}
 export class PluginManager {
+    private _config: FrameworkConfig;
     static pluginManifests: object = {};
     static plugins: object = {};
     //static pluginObjs: object = {};
     static pluginInstances: object = {};
     static errors: any = [];
+    constructor(config: FrameworkConfig) {
+        this._config = { ...config };
+    }
 
     public static async registerPlugin(manifest: Manifest, config: any) {
         PluginManager.plugins[manifest.id] = { c: config, m: manifest };
@@ -24,21 +34,23 @@ export class PluginManager {
         }
         eventManager.dispatchEvent('plugin:load', { plugin: manifest.id, version: manifest.ver });
         eventManager.dispatchEvent(manifest.id + ':load');
-        await PluginManager.instantiate();
+        //await PluginManager.instantiate(manifest);
         console.log('=====> ' + manifest.id + ' plugin loaded');
         // let p = new plugin(manifest);
         // if (manifest) {
         //     PluginManager.pluginInstances[manifest.id] = p;
         // }
     }
-
-    public static async instantiate() {
+    public static setPluginInstance(manifest: Manifest, plugin: any) {
+        PluginManager.pluginInstances[manifest.id] = new plugin();
+    }
+    /* public static async instantiate(manifest: Manifest) {
         let pluginFile = await import(this._config.pluginBasePath + manifest.id + '/Client');
         let pluginClass = <IClientPluginConstructor>pluginFile.ClientPlugin;
         let pluginInstance = new pluginClass(this._config, manifest);
         PluginManager.pluginInstances[manifest.id] = pluginInstance;
-    }
-   
+    } */
+
     public static getPluginInstance(pluginId): object {
         return PluginManager.pluginInstances[pluginId];
     }
