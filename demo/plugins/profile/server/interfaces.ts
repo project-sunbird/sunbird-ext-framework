0182d684060037b90288998ea5d510f524dcae03
Rajeev Sathish <rajeev.sathish@tarento.com>
2018-03-14 00:03:02 +0530

    Issue #000 refactor: Framework changesIssue #000 refactor: Changes for UI demo app

--- demo/plugins/profile/server/interfaces.ts
+++ demo/plugins/profile/server/interfaces.ts
@@ -1,8 +1,9 @@
-import {IServer} from 'ext-framework-server/interfaces';
+import { IServer } from 'ext-framework-server/dist/interfaces';
 import { Request, Response } from 'express';
 
 export interface IProfileService extends IServer {
 	getUser(req: Request, res: Response): void;
 	searchUsers(req: Request, res: Response): void;
 	setUser(req: Request, res: Response): void;
+	getAllUser(req: Request, res: Response): void;
 }
