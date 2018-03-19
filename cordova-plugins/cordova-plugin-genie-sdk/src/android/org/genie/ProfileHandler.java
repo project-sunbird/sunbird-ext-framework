733a4742e29c66406bd3aaf7ccd3adeec803d576
indraja <machani.indraja@gmail.com>
2018-03-19 16:03:46 +0530

    Issues #0000 feat: Added userprofile handler plugin

--- cordova-plugins/cordova-plugin-genie-sdk/src/android/org/genie/ProfileHandler.java
+++ cordova-plugins/cordova-plugin-genie-sdk/src/android/org/genie/ProfileHandler.java
@@ -30,52 +30,17 @@ import org.json.JSONException;
 
 public class ProfileHandler {
 
-    private static final String TYPE_GET_PROFILE = "getProfileById";
     private static final String TYPE_CREATE_PROFILE = "createProfile";
-    private static final String TYPE_GET_TENANT_INFO = "getTenantInfo";
-    private static final String TYPE_SEARCH_USER = "searchUser";
-    private static final String TYPE_GET_SKILLS = "getSkills";
-    private static final String TYPE_ENDORSE_OR_ADD_SKILL = "endorseOrAddSkill";
-    private static final String TYPE_SET_PROFILE_VISIBILITY = "setProfileVisibility";
-    private static final String TYPE_UPLOAD_FILE = "uploadFile";
 
     public static void handle(JSONArray args, final CallbackContext callbackContext) {
         try {
             String type = args.getString(0);
-            if (type.equals(TYPE_GET_PROFILE)) {
-                getProfileById(args, callbackContext);
-            } else if (type.equals(TYPE_GET_TENANT_INFO)) {
-                getTenantInfo(args, callbackContext);
-            } else if (type.equals(TYPE_SEARCH_USER)) {
-                searchUser(args, callbackContext);
-            } else if (type.equals(TYPE_GET_SKILLS)) {
-                getSkills(args, callbackContext);
-            } else if (type.equals(TYPE_ENDORSE_OR_ADD_SKILL)) {
-                endorseOrAddSkill(args, callbackContext);
-            } else if (type.equals(TYPE_SET_PROFILE_VISIBILITY)) {
-                setProfileVisibility(args, callbackContext);
-            } else if (type.equals(TYPE_UPLOAD_FILE)) {
+            if (type.equals(TYPE_CREATE_PROFILE)) {
                 uploadFile(args, callbackContext);
             }
         } catch (JSONException e) {
             e.printStackTrace();
         }
-    }
-
-    private static void getProfileById(JSONArray args, final CallbackContext callbackContext) throws JSONException {
-        String requestJson = args.getString(1);
-        UserProfileDetailsRequest request = GsonUtil.fromJson(requestJson, UserProfileDetailsRequest.class);
-        GenieService.getAsyncService().getUserProfileService().getUserProfileDetails(request, new IResponseHandler<UserProfile>() {
-            @Override
-            public void onSuccess(GenieResponse<UserProfile> genieResponse) {
-                callbackContext.success(genieResponse.getResult().getUserProfile());
-            }
-
-            @Override
-            public void onError(GenieResponse<UserProfile> genieResponse) {
-                callbackContext.error(genieResponse.getError());
-            }
-        });
     }
 
     /**
@@ -98,141 +63,4 @@ public class ProfileHandler {
         });
     }
 
-    /**
-     * get TenantInfo
-     */
-    private static void getTenantInfo(JSONArray args, final CallbackContext callbackContext) throws JSONException {
-        final String requestJson = args.getString(1);
-        final Gson gson = new GsonBuilder().create();
-
-        TenantInfoRequest.Builder builder = gson.fromJson(requestJson,
-                TenantInfoRequest.Builder.class);
-
-        GenieService.getAsyncService().getUserService().getTenantInfo(builder.build(), new IResponseHandler<TenantInfo>() {
-            @Override
-            public void onSuccess(GenieResponse<TenantInfo> genieResponse) {
-                callbackContext.success(genieResponse.getResult());
-            }
-
-            @Override
-            public void onError(GenieResponse<TenantInfo> genieResponse) {
-                callbackContext.error(genieResponse.getError());
-            }
-        });
-    }
-
-    /**
-     * searchUser
-     */
-    private static void searchUser(JSONArray args, final CallbackContext callbackContext) throws JSONException {
-        final String requestJson = args.getString(1);
-        final Gson gson = new GsonBuilder().create();
-
-        UserSearchCriteria.SearchBuilder builder = gson.fromJson(requestJson,
-                UserSearchCriteria.SearchBuilder.class);
-
-        GenieService.getAsyncService().getUserService().searchUser(builder.build(), new IResponseHandler<UserSearchResult>() {
-            @Override
-            public void onSuccess(GenieResponse<UserSearchResult> genieResponse) {
-                callbackContext.success(genieResponse.getResult());
-            }
-
-            @Override
-            public void onError(GenieResponse<UserSearchResult> genieResponse) {
-                callbackContext.error(genieResponse.getError());
-            }
-        });
-    }
-
-    /**
-     * getSkills
-     */
-    private static void getSkills(JSONArray args, final CallbackContext callbackContext) throws JSONException {
-        final String requestJson = args.getString(1);
-        final Gson gson = new GsonBuilder().create();
-
-        UserProfileSkillsRequest.Builder builder = gson.fromJson(requestJson,
-                UserProfileSkillsRequest.Builder.class);
-
-        GenieService.getAsyncService().getUserService().getSkills(builder.build(), new IResponseHandler<UserProfileSkill>() {
-            @Override
-            public void onSuccess(GenieResponse<UserProfileSkill> genieResponse) {
-                callbackContext.success(genieResponse.getResult());
-            }
-
-            @Override
-            public void onError(GenieResponse<UserProfileSkill> genieResponse) {
-                callbackContext.error(genieResponse.getError());
-            }
-        });
-    }
-
-    /**
-     * endorseOrAddSkill
-     */
-    private static void endorseOrAddSkill(JSONArray args, final CallbackContext callbackContext) throws JSONException {
-        final String requestJson = args.getString(1);
-        final Gson gson = new GsonBuilder().create();
-
-        EndorseOrAddSkillRequest.Builder builder = gson.fromJson(requestJson,
-                EndorseOrAddSkillRequest.Builder.class);
-
-        GenieService.getAsyncService().getUserService().endorseOrAddSkill(builder.build(), new IResponseHandler<Void>() {
-            @Override
-            public void onSuccess(GenieResponse<Void> genieResponse) {
-                callbackContext.success(genieResponse.getResult());
-            }
-
-            @Override
-            public void onError(GenieResponse<Void> genieResponse) {
-                callbackContext.error(genieResponse.getError());
-            }
-        });
-    }
-
-    /**
-     * setProfileVisibility
-     */
-    private static void setProfileVisibility(JSONArray args, final CallbackContext callbackContext) throws JSONException {
-        final String requestJson = args.getString(1);
-        final Gson gson = new GsonBuilder().create();
-
-        ProfileVisibilityRequest.Builder builder = gson.fromJson(requestJson,
-                ProfileVisibilityRequest.Builder.class);
-
-        GenieService.getAsyncService().getUserService().setProfileVisibility(builder.build(), new IResponseHandler<Void>() {
-            @Override
-            public void onSuccess(GenieResponse<Void> genieResponse) {
-                callbackContext.success(genieResponse.getResult());
-            }
-
-            @Override
-            public void onError(GenieResponse<Void> genieResponse) {
-                callbackContext.error(genieResponse.getError());
-            }
-        });
-    }
-
-    /**
-     * uploadFile
-     */
-    private static void uploadFile(JSONArray args, final CallbackContext callbackContext) throws JSONException {
-        final String requestJson = args.getString(1);
-        final Gson gson = new GsonBuilder().create();
-
-        UploadFileRequest.Builder builder = gson.fromJson(requestJson,
-                UploadFileRequest.Builder.class);
-
-        GenieService.getAsyncService().getUserService().uploadFile(builder.build(), new IResponseHandler<FileUploadResult>() {
-            @Override
-            public void onSuccess(GenieResponse<FileUploadResult> genieResponse) {
-                callbackContext.success(genieResponse.getResult());
-            }
-
-            @Override
-            public void onError(GenieResponse<FileUploadResult> genieResponse) {
-                callbackContext.error(genieResponse.getError());
-            }
-        });
-    }
 }
--- cordova-plugins/cordova-plugin-genie-sdk/www/profile.js
+++ cordova-plugins/cordova-plugin-genie-sdk/www/profile.js
@@ -4,36 +4,8 @@ var PLUGIN_NAME = 'GenieSDK';
 
 var profile = {
 
-  getProfileById: function (requestJson, success, error) {
-    exec(success, error, PLUGIN_NAME, this.action(), ["getProfileById", requestJson]);
-  },
-
   createProfile: function (requestJson, success, error) {
     exec(success, error, PLUGIN_NAME, this.action(), ["createProfile", requestJson]);
-  },
-
-  getTenantInfo: function (requestJson, success, error) {
-    exec(success, error, PLUGIN_NAME, this.action(), ["getTenantInfo", requestJson]);
-  },
-
-  searchUser: function (requestJson, success, error) {
-    exec(success, error, PLUGIN_NAME, this.action(), ["searchUser", requestJson]);
-  },
-
-  getSkills: function (requestJson, success, error) {
-    exec(success, error, PLUGIN_NAME, this.action(), ["getSkills", requestJson]);
-  },
-
-  endorseOrAddSkill: function (requestJson, success, error) {
-    exec(success, error, PLUGIN_NAME, this.action(), ["endorseOrAddSkill", requestJson]);
-  },
-
-  setProfileVisibility: function (requestJson, success, error) {
-    exec(success, error, PLUGIN_NAME, this.action(), ["setProfileVisibility", requestJson]);
-  },
-
-  uploadFile: function (requestJson, success, error) {
-    exec(success, error, PLUGIN_NAME, this.action(), ["uploadFile", requestJson]);
   },
 
   action: function () {
