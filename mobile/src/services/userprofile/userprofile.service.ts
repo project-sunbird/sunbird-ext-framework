8f4056239eae846c8a7ec35768ed018b27fce814
Shriharsh <shriharshs@gmail.com>
2018-03-21 16:39:53 +0530

    Issue #24 chore: Refactoring the userprofile.service.ts

--- mobile/src/services/userprofile/userprofile.service.ts
+++ mobile/src/services/userprofile/userprofile.service.ts
@@ -6,10 +6,10 @@ import {UserProfileDetailsRequest, UserProfileSkillsRequest} from "./bean"
 export class UserProfileService {
 
     getUserProfileDetails(request: UserProfileDetailsRequest, onSuccess, onError) {
-        (<any>window).GenieSDK.userprofile.getUserProfileDetails(JSON.stringify(request), onSuccess, onError);
+        (<any>window).GenieSDK.userProfile.getUserProfileDetails(JSON.stringify(request), onSuccess, onError);
     }
 
     getSkills(request: UserProfileSkillsRequest, onSuccess, onError) {
-        (<any>window).GenieSDK.userprofile.getSkills(JSON.stringify(request), onSuccess, onError);
+        (<any>window).GenieSDK.userProfile.getSkills(JSON.stringify(request), onSuccess, onError);
     }
 }
