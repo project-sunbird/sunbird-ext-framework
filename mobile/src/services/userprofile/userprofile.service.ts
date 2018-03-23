04d6d6ace0fc4b1462a3338076b48c2c6aa6c9d9
indraja <machani.indraja@gmail.com>
2018-03-23 16:15:08 +0530

    Issus #0000 feat: Added userprofile related methods in userprofileservice ts file

--- mobile/src/services/userprofile/userprofile.service.ts
+++ mobile/src/services/userprofile/userprofile.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from "@angular/core";
 
-import {UserProfileDetailsRequest, UserProfileSkillsRequest} from "./bean"
+import {
+    UserProfileDetailsRequest, UserProfileSkillsRequest, TenantInfoRequest,
+    UserSearchCriteria, EndorseOrAddSkillRequest, ProfileVisibilityRequest, UploadFileRequest
+} from "./bean"
 
 @Injectable()
 export class UserProfileService {
@@ -9,7 +12,28 @@ export class UserProfileService {
         (<any>window).GenieSDK.userprofile.getUserProfileDetails(JSON.stringify(request), onSuccess, onError);
     }
 
+    getTenantInfo(request: TenantInfoRequest, onSuccess, onError) {
+        (<any>window).GenieSDK.userprofile.getTenantInfo(JSON.stringify(request), onSuccess, onError);
+    }
+
+    searchUser(request: UserSearchCriteria, onSuccess, onError) {
+        (<any>window).GenieSDK.userprofile.searchUser(JSON.stringify(request), onSuccess, onError);
+    }
+
     getSkills(request: UserProfileSkillsRequest, onSuccess, onError) {
         (<any>window).GenieSDK.userprofile.getSkills(JSON.stringify(request), onSuccess, onError);
     }
+
+    endorseOrAddSkill(request: EndorseOrAddSkillRequest, onSuccess, onError) {
+        (<any>window).GenieSDK.userprofile.endorseOrAddSkill(JSON.stringify(request), onSuccess, onError);
+    }
+
+    setProfileVisibility(request: ProfileVisibilityRequest, onSuccess, onError) {
+        (<any>window).GenieSDK.userprofile.setProfileVisibility(JSON.stringify(request), onSuccess, onError);
+    }
+
+    uploadFile(request: UploadFileRequest, onSuccess, onError) {
+        (<any>window).GenieSDK.userprofile.uploadFile(JSON.stringify(request), onSuccess, onError);
+    }
+
 }
