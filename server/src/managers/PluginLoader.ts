db349db7c7b5930f39d5bdf9c41a328dc8237c48
sunil <sunils@ilimi.in>
2018-04-13 11:19:52 +0530

    Issue #52 refactor: framework changes
    - added: test cases for pluginLoader
    - changes: PluginRegistry test cases
    - changes: PluginManager test cases

--- server/src/managers/PluginLoader.ts
+++ server/src/managers/PluginLoader.ts
@@ -26,18 +26,18 @@ export class PluginLoader {
     }
 
     public getPluginManifest(pluginId: string): Manifest {
-		return this._pluginInstances.find((plugin) => {
-			return plugin.id === pluginId;
-		});
-	}
+        return this._pluginInstances.find((plugin) => {
+            return plugin.id === pluginId;
+        });
+    }
 
-	public getPluginInstance(pluginId: string) : any {
-		return this._pluginInstances[pluginId];
-	}
+    public getPluginInstance(pluginId: string): any {
+        return this._pluginInstances[pluginId];
+    }
 
     private async loadDependencies(manifest: Manifest) {
         for (let dependency of manifest.server.dependencies) {
-            if (this._pluginsLoaded.indexOf(dependency.id) == -1) {
+            if (this._pluginsLoaded.indexOf(dependency.id) === -1) {
                 await this.loadPlugin(dependency);
             }
         }
@@ -56,29 +56,47 @@ export class PluginLoader {
      * @param plugin IPlugin
      */
     public async loadPlugin(plugin: IPlugin) {
-        let instance = this;
-        this._pluginsLoaded.push(plugin.id); // Step 1
+        if (this.isPluginLoaded(plugin.id)) return;
+        this.register(plugin.id); // Step 1
         const manifest = await this.getManifest(plugin); // Step 2
-        const pluginManifest = _.cloneDeep(manifest);
-        if (typeof (manifest.server.dependencies) !== undefined) { // Step 3
-            await this.loadDependencies(pluginManifest);
-        }
-        await Promise.all([PluginRegistry.register(pluginManifest), // Step 4
-            instance.preparePlugin(pluginManifest), // Step 5
-            instance.instantiatePlugin(pluginManifest), // Step 6
-            instance.registerRoutes(pluginManifest) // Step 7
-        ])
+        if (manifest.server.dependencies) await this.loadDependencies(manifest); //step 3
+        await PluginRegistry.register(manifest) // Step 4
+        await this.buildPlugin(manifest).catch((error) => {
+            this.unregister(plugin.id)
+            throw new FrameworkError({ message: 'plugin load failed ', code: FrameworkErrors.PLUGIN_LOAD_FAILED, rootError: error });
+        })
+    }
+
+    public async buildPlugin(manifest) {
+        await Promise.all([
+            this.preparePlugin(manifest), // Step 5
+            this.instantiatePlugin(manifest), // Step 6
+            this.registerRoutes(manifest) // Step 7
+        ]).catch((error) => {
+            throw new FrameworkError({ message: 'plugin build failed ', code: FrameworkErrors.PLUGIN_BUILD_FAILED, rootError: error });
+        })
+    }
+
+    private register(pluginId: string) {
+        this._pluginsLoaded.push(pluginId);
+    }
+
+    private unregister(pluginId: string) {
+        _.remove(this._pluginsLoaded, (loadedPluginId) => loadedPluginId === pluginId)
+    }
+
+    public isPluginLoaded(pluginId: string) {
+        if (this._pluginsLoaded.indexOf(pluginId) === -1) return false;
+        return true;
     }
 
     private async getManifest(plugin: IPlugin) {
-        try {
-            //TODO: Look for multiple paths
-            const pluginId = plugin.id;
-            const pluginManifest = await import(this.config.pluginBasePath + pluginId + '/manifest');
-            return Manifest.fromJSON(<IPluginManifest>pluginManifest.manifest);
-        } catch (err) {
-            throw new FrameworkError({ code: FrameworkErrors.MANIFEST_NOT_FOUND, rootError: err });
-        }
+        //TODO: Look for multiple paths
+        const pluginId = plugin.id;
+        const pluginManifest = await import(this.config.pluginBasePath + pluginId + '/manifest').catch((error) => {
+            throw new FrameworkError({ message: `manifest not found for the plugin ${plugin.id}`, code: FrameworkErrors.MANIFEST_NOT_FOUND, rootError: error });
+        })
+        return Manifest.fromJSON(<IPluginManifest>pluginManifest.manifest);
     }
 
     private async preparePlugin(manifest: Manifest) {
@@ -91,7 +109,9 @@ export class PluginLoader {
 
     private async instantiatePlugin(manifest: Manifest) {
         try {
-            let pluginFile = await import(this.config.pluginBasePath + manifest.id + '/server');
+            let pluginFile = await import(this.config.pluginBasePath + manifest.id + '/server').catch((error) => {
+                throw new FrameworkError({ message: `Entry file not found for the plugin ${manifest.id}`, code: FrameworkErrors.ENTRY_FILE_NOT_FOUND, rootError: error });
+            });
             let pluginClass = <IServerConstructor>pluginFile.Server;
             let pluginInstance = new pluginClass(this.config, manifest);
             this._pluginInstances[manifest.id] = pluginInstance;
@@ -103,25 +123,26 @@ export class PluginLoader {
     private async registerRoutes(manifest: Manifest) {
         try {
             let router = RouterRegistry.bindRouter(manifest);
-            let pluginRouter = await import(this.config.pluginBasePath + manifest.id + '/routes');
+            let pluginRouter = await import(this.config.pluginBasePath + manifest.id + '/routes').catch((error) => {
+                throw new FrameworkError({ message: `Router file not found for the plugin ${manifest.id}`, code: FrameworkErrors.ROUTER_FILE_NOT_FOUND, rootError: error });
+            });
             pluginRouter = <IRouterConstructor>pluginRouter.Router;
             const routerInstance = new pluginRouter();
             routerInstance.init(router, manifest);
         } catch (err) {
-            throw new FrameworkError({ code: FrameworkErrors.ROUTE_REGISTRY_FAILED, rootError: err });
+            throw new FrameworkError({ code: FrameworkErrors.PLUGIN_ROUTE_INIT_FAILED, rootError: err });
         }
     }
 
     public static async loadDBSchema(manifest: Manifest, path: string) {
-        let files = glob.sync(path, {});
-        for (let path of files) {
-            try {
-                let schema = await import(path);
-                let schemaLoader = <ISchemaLoader>SchemaLoader.getLoader(schema.type);
-                await schemaLoader.create(manifest.id, schema);
-            } catch (error) {
+        for (let schemaFilePath of glob.sync(path, {})) {
+            let schema = await import(schemaFilePath).catch((error) => {
+                throw new FrameworkError({ message: `invalid schema file path for plugin ${manifest.id}`, code: FrameworkErrors.INVALID_SCHEMA_PATH, rootError: error });
+            });
+            let schemaLoader = <ISchemaLoader>SchemaLoader.getLoader(schema.type);
+            await schemaLoader.create(manifest.id, schema).catch((error) => {
                 throw new FrameworkError({ message: `Error while laoding DB schema for plugin ${manifest.id}`, code: FrameworkErrors.SCHEMA_LOADER_FAILED, rootError: error });
-            }
+            });
         }
     }
 }
