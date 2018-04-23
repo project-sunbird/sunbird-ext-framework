4ef4f64a395030eb551eee4540f18676548b4efd
sunil <sunils@ilimi.in>
2018-04-23 15:24:03 +0530

    Issue #000 update: use case [server plugin]
    changes:
    - added cassandra service use case  for profile plugin

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -3,15 +3,27 @@
  */
 import { PluginManager } from "../managers/PluginManager";
 import { FrameworkConfig } from "../interfaces";
-import { CassandraDB } from "../db";
+import { CassandraDB, cassandraDriver, ElasticSearchDB } from "../db";
+import { RouterRegistry } from "../managers/RouterRegistry";
+import { Util } from "../util";
 
 export class FrameworkAPI {
     constructor(private config: FrameworkConfig) {
         this.config = config;
     }
 
-    public getCassandraInstance() {
-        return new CassandraDB(this.config.db.cassandra)
+    public getCassandraInstance(pluginId: string, schema: any): cassandraDriver.Client {
+        let instance = new CassandraDB(this.config.db.cassandra);
+        return instance.getConnectionByKeyspace(Util.generateId(pluginId, schema.db));
+    }
+
+    public getElasticsearchInstance(pluginId: string, schema: any) {
+        let instance = new ElasticSearchDB();
+        return instance.getConnection(pluginId, true);
+    }
+
+    public threadLocal() {
+        return RouterRegistry.getThreadNamespace();
     }
 }
 
