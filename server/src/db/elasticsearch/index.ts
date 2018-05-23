a9c40b536a96f3802a521289e17fb1d25e1018c6
sunil <sunils@ilimi.in>
2018-05-23 18:41:20 +0530

    Issue #000 update: server
    changes:
    - refactor: proxymethod call for Elasticsearch

--- server/src/db/elasticsearch/index.ts
+++ server/src/db/elasticsearch/index.ts
@@ -9,25 +9,6 @@ import { Util } from '../../util';
 import { IElasticSearchConfig, IElasticSearchConnector } from "../../interfaces";
 import { logger } from '../../logger';
 
-function proxyMethodCalls(obj: IElasticSearchConnector, disabledAPIs: Array<string>) {
-  let handler = {
-    get(target, propKey, receiver) {
-      if (disabledAPIs.indexOf(propKey) !== -1) {
-        logger.error(`ElasticsearchAPI: ${propKey}: this api is disabled!`);
-        return Object.create({});
-      }
-      if (typeof target[propKey] === 'object' && target[propKey] !== null) {
-        return new Proxy(target[propKey], handler)
-      }
-      const originalMethod = target[propKey];
-      return function () {
-        return originalMethod.apply(target, arguments);
-      }
-    }
-  };
-  return new Proxy(obj, handler);
-}
-
 export class ElasticSearchDB {
 
   private static _config: IElasticSearchConfig;
@@ -41,7 +22,26 @@ export class ElasticSearchDB {
   public static getConnection(pluginId: string, proxy?: boolean): IElasticSearchConnector {
     // Elasticsearch.js allows only one instance of 'Client'
     // src: https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/configuration.html
-    ElasticSearchDB._client = ElasticSearchDB._client || proxyMethodCalls(new elasticsearch.Client(ElasticSearchDB._config), ElasticSearchDB._config.disabledApis);
+    ElasticSearchDB._client = ElasticSearchDB._client || ElasticSearchDB.proxyMethodCalls(new elasticsearch.Client(ElasticSearchDB._config), ElasticSearchDB._config.disabledApis);
     return ElasticSearchDB._client;
+  }
+
+  private static proxyMethodCalls(obj: IElasticSearchConnector, disabledAPIs: Array<string>) {
+    let handler = {
+      get(target, propKey, receiver) {
+        if (disabledAPIs.indexOf(propKey) !== -1) {
+          logger.error(`ElasticsearchAPI: ${propKey}: this api is disabled!`);
+          return Object.create({});
+        }
+        if (typeof target[propKey] === 'object' && target[propKey] !== null) {
+          return new Proxy(target[propKey], handler)
+        }
+        const originalMethod = target[propKey];
+        return function () {
+          return originalMethod.apply(target, arguments);
+        }
+      }
+    };
+    return new Proxy(obj, handler);
   }
 };
