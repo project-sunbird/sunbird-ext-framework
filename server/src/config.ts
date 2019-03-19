d342d10ecd910ac44db78af9ec5e8b881d1c0490
Harishkumar Gangula <harishg@ilimi.in>
2019-03-19 18:30:18 +0530

    Issue #SB-10822 feat:  indentation error

--- server/src/config.ts
+++ server/src/config.ts
@@ -4,26 +4,26 @@
 import { FrameworkConfig } from './interfaces'
 
 export const defaultConfig: FrameworkConfig = {
-  db: {
-    cassandra: {
-      contactPoints: ['127.0.0.1'],
-      defaultKeyspaceSettings: {
-        replication: {
-          'class': 'SimpleStrategy',
-          'replication_factor': '1'
+    db: {
+        cassandra: {
+            contactPoints: ['127.0.0.1'],
+            defaultKeyspaceSettings: {
+                replication: {
+                    'class': 'SimpleStrategy',
+                    'replication_factor': '1'
+                }
+            }
+        },
+        elasticsearch: {
+            host: '127.0.0.1:9200',
+            disabledApis: ['cat', 'cluster', 'ingest', 'nodes', 'remote', 'snapshot', 'tasks']
+        },
+        couchdb: {
+            url: 'http://127.0.0.1:5984',
         }
-      }
     },
-    elasticsearch: {
-      host: '127.0.0.1:9200',
-      disabledApis: ['cat', 'cluster', 'ingest', 'nodes', 'remote', 'snapshot', 'tasks']
-    },
-    couchdb: {
-      url: 'http://127.0.0.1:5984',
-    }
-  },
-  plugins: [],
-  pluginBasePath: '',
-  logLevel: 'debug',
-  port: 9000 // default
+    plugins: [],
+    pluginBasePath: '',
+    logLevel: 'debug',
+    port: 9000 // default
 }
