71d4c7cdb94a4c341e251034047bfac45156e7b8
Rajeev Sathish <rajeev.sathish@tarento.com>
2020-07-04 22:44:28 +0530

    Issue #SB-19620 Form list API to provide the list of all forms attribute for a Tenant

--- demo/plugins/FormService/server/routes.ts
+++ demo/plugins/FormService/server/routes.ts
@@ -12,5 +12,6 @@ export class Router implements IRouter {
 		app.post('/read', validator.validateReadAPI, (req, res) => { server.read(req, res) });
     app.post('/update', validator.validateUpdateAPI, (req, res) => { server.update(req, res) });
     app.post('/create', validator.validateCreateAPI, (req, res) => { server.create(req, res) });
+    app.post('/list', (req, res) => { server.listAll(req, res) });
 	}
 }
