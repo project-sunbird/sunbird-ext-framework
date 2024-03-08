88741df2223dfbc025868e8fbeafd649ac071d8b
Abhishek Nagesh <abhishek.nagesh@tarento.com>
2024-03-08 11:55:58 +0530

    Issue #ED-3685: Updated web extension to ng14

--- web-extensions/sunbird-web-extensions-app/projects/sunbird-web-extension/src/public_api.ts
+++ web-extensions/sunbird-web-extensions-app/projects/sunbird-web-extension/src/public_api.ts
@@ -2,4 +2,9 @@
  * Public API Surface of sunbird-web-extension
  */
 
-export * from './index';
+export * from './app.module';
+export * from './BootstrapFramework';
+export * from './extension-point.directive';
+export * from './models';
+export * from './plugin-service';
+
